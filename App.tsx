8fc1b309e89ed25dc783348ea7f334befc24001f
bbawuah <brianbawuah@icloud.com>
2020-08-31 19:39:53 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -6,12 +6,16 @@
  * @flow strict-local
  */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 import { MainNavigator } from './src/domain/MainNavigator/MainNavigator'
+import SplashScreen from 'react-native-splash-screen'
 
 const App: React.FC = () => {
+    useEffect(() => {
+        SplashScreen.hide()
+    }, [])
     return (
         <NavigationContainer>
             <MainNavigator />
